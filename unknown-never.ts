7f71ef9bceb7bf097c23d758a2ddba5281b03cec
kimminkyu <kmk50009@naver.com>
2022-02-15 00:40:35 +0900

    Add unknown-never.ts

---
+++ unknown-never.ts
@@ -0,0 +1,16 @@
+{
+  let userInput: unknown;
+  let userName: string;
+
+  userInput = 5;
+  userInput = "elliot";
+  if (typeof userInput === "string") {
+    userName = userInput;
+  }
+
+  function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
+  }
+
+  generateError("An error occurred!", 500);
+}
