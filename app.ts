ec1ffc5781f2f474d94b7c9eacc692a2b8a2ca51
kimminkyu <kmk50009@naver.com>
2022-02-14 21:46:58 +0900

    Add enum

--- app.ts
+++ app.ts
@@ -1,20 +1,36 @@
 {
-  const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-  } = {
+  // const person: {
+  //   name: string;
+  //   age: number;
+  //   hobbies: string[];
+  //   role: [number, string];
+  // } = {
+  //   name: "elliot",
+  //   age: 30,
+  //   hobbies: ["sports", "cooking"],
+  //   role: [2, "author"],
+  // };
+
+  // let favoriteActivities: string[];
+  // favoriteActivities = ["Sports"];
+
+  // for (const hobby of person.hobbies) {
+  //   console.log(hobby);
+  // }
+  enum Role {
+    ADMIN='ADMIN',
+    READ_ONLY= 100,
+    AUTHOR,
+  }
+
+  const person = {
     name: "elliot",
     age: 30,
     hobbies: ["sports", "cooking"],
-    role: [2, "author"],
+    role: Role.ADMIN,
   };
 
-  let favoriteActivities: string[];
-  favoriteActivities = ["Sports"];
-
-  for (const hobby of person.hobbies) {
-    console.log(hobby);
+  if(person.role === Role.AUTHOR) {
+    console.log('is author')
   }
 }
