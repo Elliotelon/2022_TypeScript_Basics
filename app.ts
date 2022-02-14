fa1481cb1b4bd1ad32e732d39984f5366f9caccc
kimminkyu <kmk50009@naver.com>
2022-02-14 23:54:45 +0900

    Add Alias

--- app.ts
+++ app.ts
@@ -1,15 +1,35 @@
-function combine(input1: number | string, input2: number | string) {
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
+
+  //   if (resultConversion === "as-number") {
+  //     return +result;
+  //   } else {
+  //     return result.toString();
+  //   }
+
   return result;
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine('Max', "Anna");
-console.log(combinedNames)
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
